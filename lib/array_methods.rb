d27421899fa76a55a32b0b7f9d6d80ed3b6326af
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-15 21:14:08 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  for i in array 
+  for i in array do
+    if array[i] == value_to_find 
+  end
 end
 
 def find_max_value(array)
