236c708bfd5629e1002d5c5e34627505521acc3c
Ryan Wilson <rjwilson@terpmail.umd.edu>
2020-01-15 21:17:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def find_element_index(array, value_to_find)
   # Add your solution here
-  for i in array do
+  for i in 0..array.length do
     if array[i] == value_to_find 
       return i
     end
